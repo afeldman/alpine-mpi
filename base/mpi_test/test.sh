9c27357e7fadc8e8ea537880457e877d42079a11
anton feldmann <anton.feldmann@gmail.com>
2023-11-10 10:24:32 +0100

    start project

---
+++ base/mpi_test/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+echo "--- Test MPI installation ---"
+
+printf "it should find mpicc... "
+mpicc --version > /dev/null
+echo ok
+
+printf "it should find mpiexec... "
+mpiexec --version > /dev/null
+echo ok
+
+printf "it should compile mpi_hello_world.c source... "
+mpicc -o mpi_hello_world mpi_hello_world.c > /dev/null
+echo ok
+
+printf "it should run mpi_hello_world program successfully... "
+mpirun -n 4 ./mpi_hello_world > /dev/null
+echo ok
+
+export OMPI_ALLOW_RUN_AS_ROOT=0
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=0
